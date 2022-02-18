e52a353b0ffedeb5fe81bce3531c8d2a51f13213
s10i <s10i@localhost.localdomain>
2022-02-18 20:19:05 +0330

    update authentication endpoint

--- authz/config/config.py
+++ authz/config/config.py
@@ -13,6 +13,8 @@ class Config:
 	SECRET_KEY = environ.get("TECHLAND_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-HARD-SECRET-KEY")
 	
 	TIMEZONE = environ.get("TECHLAND_AUTHZ_TIMEZONE", "Asia/Tehran")
+	
+	JWT_ALGO = environ.get("TECHLAND_AUTHZ_JWT_ALGO", "HS512")
 
 	##################### Database Configs ###########################
 	
@@ -26,8 +28,16 @@ class Config:
 
 	##################### User Configs ###########################
 	
+	USER_ENABLED_STATUS = 1
+	
+	USER_ACTIVATED_STATUS = 2
+	
+	USER_ALL_STATUS = 3
+	
 	USER_DEFAULT_ROLE = environ.get("TECHLAND_AUTHZ_USER_DEFAULT_ROLE", "member")
 	
 	USER_DEFAULT_EXPIRY_TIME = int(environ.get("TECHLAND_AUTHZ_USER_DEFAULT_EXPIRY_TIME", "365"))
 	
 	USER_DEFAULT_STATUS = int(environ.get("TECHLAND_AUTHZ_USER_DEFAULT_STATUS", "3"))
+	
+	USER_DEFAULT_TOKEN_EXPIRY_TIME = int(environ.get("TECHLAND_AUTHZ_USER_DEFAULT_TOKEN_EXPIRY_TIME", "86400"))
